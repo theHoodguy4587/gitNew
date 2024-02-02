311d268299caaa93b9d1518de7b789b06c6cd849
theHoodguy4587 <senitha02@gmail.com>
2024-02-03 00:12:02 +0530

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+crush = input("who's ur crush: ")
+
+
+# In[2]:
+
+
+name = input("ur name: ")
+
+
+# In[11]:
+
+
+madlib = f"your name is {name} and your crush is {crush} "
+
+
+# In[12]:
+
+
+print(madlib)
+
+
+# In[ ]:
+
+
+
+
